91d1bc2de92a84885308b78017bc5be3acffb8c8
Rishabh Rao <rishabh.ink+github@gmail.com>
2021-08-11 18:53:51 +0200

    Jest uses the 'global' object in its test environment, hence use it instead of 'window'

--- test/matchUserPreference.test.js
+++ test/matchUserPreference.test.js
@@ -6,7 +6,7 @@ import mockWindowMatchMedia from '../testing/mockWindowMatchMedia';
 
 describe('matchUserPreference()', () => {
   it('returns true if media query matches', () => {
-    window.matchMedia = jest
+    global.matchMedia = jest
       .fn()
       .mockImplementation(() => mockWindowMatchMedia(true, `(prefers-reduced-motion: reduce)`));
 
@@ -14,7 +14,7 @@ describe('matchUserPreference()', () => {
   });
 
   it('returns false if media query does not match', () => {
-    window.matchMedia = jest
+    global.matchMedia = jest
       .fn()
       .mockImplementation(() => mockWindowMatchMedia(false, `(prefers-reduced-motion: reduce)`));
 
@@ -22,7 +22,7 @@ describe('matchUserPreference()', () => {
   });
 
   it('returns false if media query does not match but media query string matches', () => {
-    window.matchMedia = jest.fn().mockImplementation(() => mockWindowMatchMedia(true, `not all`));
+    global.matchMedia = jest.fn().mockImplementation(() => mockWindowMatchMedia(true, `not all`));
 
     expect(matchUserPreference('prefers-reduced-motion', 'reduce')).toBe(false);
   });
